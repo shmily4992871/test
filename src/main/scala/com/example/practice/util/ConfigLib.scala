ac1e177456bf2919f6cfe69a19cd68f6c23e04a2
shmily4992871 <w15182915802@163.com>
2019-01-07 10:35:18 +0800

    first commit

---
+++ src/main/scala/com/example/practice/util/ConfigLib.scala
@@ -0,0 +1,43 @@
+package com.example.practice.util
+
+import io.lemonlabs.uri.Url
+import Url._
+
+import scala.util.control.Exception._
+
+trait ConfigLib {
+  def getConfig[T](k: String)(implicit dataConverter: DataConverter[T]): Option[T]
+}
+
+object AppConfigLib extends ConfigLib {
+  def getConfig[T](k: String)(implicit dc: DataConverter[T]): Option[T] =
+    Option(System.getenv(k)).flatMap(dc.convert)
+}
+
+trait DataConverter[T] {
+  def convert(v: String): Option[T]
+}
+
+class IntDataConverter extends DataConverter[Int] {
+  def convert(v: String): Option[Int] = allCatch.opt(v.toInt)
+}
+
+class StringDataConverter extends DataConverter[String] {
+  def convert(v: String): Option[String] = Some(v)
+}
+
+class BooleanDataConverter extends DataConverter[Boolean] {
+  def convert(v: String): Option[Boolean] = allCatch.opt(v.toBoolean)
+}
+
+// TODO - should handle the exception cases
+class UriDataConverter extends DataConverter[Url] {
+  def convert(v: String): Option[Url] = allCatch.opt(parse(v))
+}
+
+object DataConverter {
+  implicit val intConverter  = new IntDataConverter()
+  implicit val strConverter  = new StringDataConverter()
+  implicit val boolConverter = new BooleanDataConverter()
+  implicit val uriConverter  = new UriDataConverter()
+}
